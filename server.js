b1e8a8f588d27c6687fb07d69ddd733f83635f0c
Ashley Radford <aradford7@outlook.com>
2019-04-23 15:09:33 -0700

    houseController in server

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+//Set up modules here
 const express = require('express');
 const app = express();
-require('dotenv').config()
-const controller = require('./controller')
+const bodyParser = require('body-parser');
+const methodOverride =  require('method-override');
+//const session = require('express-session');
 
+//require modules here
+require('./db/db');
 
-app.use('/', controller)
+const housesController = require('./controllers/houses');
 
+//MiddleWare Area
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
 
-app.listen(3000,()=>{
-  console.log('running on port:', 3000)
+// app.use(sessions({
+//   secret: 'Random Secret String',
+//   resave: false,
+//   saveUninitalized: false,
+// }));
+
+app.use('/houses', housesController);
+
+app.listen(PORT, (err) => {
+  console.log('Server listening on port: ', PORT);
 });
