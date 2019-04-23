0136e469c0fd1c75a547caca270a188fc739d8a6
Anis Mansour <anismansour@Aniss-MacBook-Pro.local>
2019-04-23 15:54:21 -0700

    newserver

--- server.js
+++ server.js
@@ -1,40 +1,25 @@
-<<<<<<< HEAD
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const realtorsController = require("./controller/realtors");
-
-app.use("/", realtorsController);
-
-app.listen(3000, () => {
-  console.log("running on port:", 3000);
-});
-=======
 //Set up modules here
 const PORT = 3000;
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const bodyParser = require('body-parser');
-const methodOverride =  require('method-override');
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 //const session = require('express-session');
 
-require('dotenv').config()
-const houseController = require('./controller')
-
+require("dotenv").config();
+const houseController = require("./controller");
 
 //require modules here
-require('./db/db');
+require("./db/db");
 
+const housesController = require("./controllers/houses");
 
-const housesController = require('./controllers/houses');
-
-app.use('/', houseController)
-
+app.use("/", houseController);
 
 //MiddleWare Area
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 // app.use(sessions({
 //   secret: 'Random Secret String',
@@ -42,9 +27,8 @@ app.use(methodOverride('_method'));
 //   saveUninitalized: false,
 // }));
 
-app.use('/houses', housesController);
+app.use("/houses", housesController);
 
-app.listen(PORT, (err) => {
-  console.log('Server listening on port: ', PORT);
+app.listen(PORT, err => {
+  console.log("Server listening on port: ", PORT);
 });
->>>>>>> master
