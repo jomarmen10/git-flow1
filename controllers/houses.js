56ad752d6536a3c7c27388b958ea43605061d0e3
Ashley Radford <aradford7@outlook.com>
2019-04-23 16:49:50 -0700

    added

--- controllers/houses.js
+++ controllers/houses.js
@@ -11,26 +11,26 @@ router.get('/new', (req,res) => {
 //index route get route and post
 router.get('/', (req,res) => {
     //res.render('houses/index.ejs');
-    House.find({}, (err, foundHouses) => {
-        if (err) {
-            res.send(err);
-        }else{
+    // House.find({}, (err, foundHouses) => {
+    //     if (err) {
+    //         res.send(err);
+    //     }else{
             res.render('houses/index.ejs', {
-                houses: foundHouses
+               /// houses: foundHouses
             });
-        }
-    });
+       // }
+   // });
 });
 
 router.post('/', (req,res) => {
-    House.create(req.body, (err, createdHouse) => {
-        if(err){
-            res.send(err);
-        }else{
+    // House.create(req.body, (err, createdHouse) => {
+    //     if(err){
+    //         res.send(err);
+    //     }else{
             res.redirect('/houses');
-        }
+        //}
     });
-});
+// });
 //show route
 router.get('/:id', (req,res) => {
     House.findById(req.params.id, (err, foundHouse) => {
@@ -38,44 +38,44 @@ router.get('/:id', (req,res) => {
             res.send(err);
         }else{
             res.render('houses/show.ejs', {
-                house:foundHouse
+              house:foundHouse
             });
-        }
+       }
     });
-});
+ });
 
 //Edit route get and put req
 router.get('/:id/edit', (req,  res) => {
-    House.findById(req.params.id, (err, foundHouse) => {
-        if(err){
-            res.send(err);
-        }else{
+    // House.findById(req.params.id, (err, foundHouse) => {
+    //     if(err){
+    //         res.send(err);
+    //     }else{
             res.render('houses/edit.ejs', {
                 house:foundHouse
             });
-        }
+       // }
     });
-});
+//});
 router.put('/:id', (req, res)=> {
-    House.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, foundHouse) => {
-        if(err){
-            res.send(err);
-        }else{
+    // House.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, foundHouse) => {
+    //     if(err){
+    //         res.send(err);
+    //     }else{
             res.redirect('/houses');
-        }
+        //}
     });
-});
+//});
 
 //delete route
 router.delete('/:id', (req,res) => {
-    House.findByIdAndRemove(req.params.id, (err, deletedHouse) => {
-        if(err){
-            res.send(err);
-        }else{
+    // House.findByIdAndRemove(req.params.id, (err, deletedHouse) => {
+    //     if(err){
+    //         res.send(err);
+    //     }else{
             res.redirect('/houses');
-        }
+       // }
     });
-});
+//});
 
 
 module.exports = router;
