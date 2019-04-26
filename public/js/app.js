0ec97d58afe32f965d70787f9332c8ccb6353cb8
Anis Mansour <anismansour@Aniss-MacBook-Pro.local>
2019-04-26 16:09:04 -0700

    added filter map

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,2 @@
-const House = require('../../models/houses')
-const Realtor = require('../../models/realtors')
-
-
-let price = [
-  {
-    "100k":100000
-  },
-  {
-    "200k":200000
-  }
-]
-
-
-const getPrice = ()=>{
-
-}
-
-
-console.log(price[0]['100k'])
-
-console.log(Realtor.name)
+// const House = require("../../models/houses");
+// const Realtor = require("../../models/realtors");
