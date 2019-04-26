1883615bca4417be84aa08f822de749116bda2ea
Jomar Mendoza <Omay@Macintosh-2.local>
2019-04-26 16:16:25 -0700

    fillter for name works

--- public/js/map.js
+++ public/js/map.js
@@ -12,28 +12,7 @@ function initMap() {
   };
   //new map
   let map = new google.maps.Map(document.getElementById("map"), options);
-  /*
-//add marker
-let marker = new google.maps.Marker({
-    position:{lat:34.046698, lng:-118.2426782},
-    map:map
-});
-let infoWindow = new google.maps.InfoWindow({
-    content: '<h1>General Assembly</h1>'
-});
-marker.addListener('click', function(){
-    infoWindow.open(map, marker);
-});
-*/
 
-  addMarker({
-    coords: { lat: 34.046698, lng: -118.2426782 },
-    content: "<h1>General Assembly</h1>"
-  });
-  addMarker({
-    coords: { lat: 34.0430175, lng: -118.2694428 },
-    content: "<h1>Staple Center</h1>"
-  });
 
   displayHouses(addMarker);
   //FUNCTION  FOR SERVERAL MARKERS WITHOUT HARD CODE to be added in the loop
@@ -61,9 +40,6 @@ marker.addListener('click', function(){
 
 // get location
 let locationForm = document.getElementById("location-form");
-// let locationInput = document.querySelector('#location-input')
-
-// listin for submit location
 
 locationForm.addEventListener("submit", function(e) {
   e.preventDefault();
@@ -75,7 +51,6 @@ let longitude;
 let latitude;
 function geocode() {
   let location = document.getElementById("location-input").value;
-  // let location ='630 masselin avenue Los Angeles';
   axios
     .get("https://maps.googleapis.com/maps/api/geocode/json", {
       params: {
@@ -84,67 +59,43 @@ function geocode() {
       }
     })
     .then(function(response) {
-      //  show object of address
-      // console.log(response);
 
       //formated address
       let formattedAddress = response.data.results[0].formatted_address;
       let formattedAddressOutput = `
-<ul class="list-group">
-<li class= "list-group-item">${formattedAddress}</li>
-</ul>
-`;
+        <ul class="list-group">
+          <li class= "list-group-item">${formattedAddress}</li>
+        </ul>
+        `;
       //address components
-
       let addressComponents = response.data.results[0].address_components;
       let addressComponentsOutput = '<ul class="list-group">';
       for (let i = 0; i < addressComponents.length; i++) {
         addressComponentsOutput += `
-  <li class="list-group-item">${addressComponents[i].types[0]}:${
-          addressComponents[i].long_name
-        }</li>
-  `;
+          <li class="list-group-item">${addressComponents[i].types[0]}:${
+            addressComponents[i].long_name
+          }</li>
+        `;
       }
-
       //geo
       lat = response.data.results[0].geometry.location.lat;
       lng = response.data.results[0].geometry.location.lng;
 
-      // options = {
-      //   zoom: 10,
-      //   center: {lat, lng}
-      // }
-      //////////!!!!!!!!!!!!!!!!!!!!!!!!!/////
-
       initMap();
-      // console.log(lat);
-
       let geometryOutput = `
-<ul class="list-group">
-<li class= "list-group-item"><strong>latitude</strong>:${lat}</li>
-<li class= "list-group-item"><strong>longitude</strong>:${lng}</li>
-
-</ul>
-`;
-
-      //show the list of the geo function with all the info inside the object
-
-      //output to appdocument.getElementById('formatted-address').innerHTML =
-      //formattedAddressOutput;
-      // document.getElementById('address-components').innerHTML =
-      // addressComponentsOutput;
-      // document.getElementById('geometry').innerHTML =
-      // geometryOutput;
+        <ul class="list-group">
+          <li class= "list-group-item"><strong>latitude</strong>:${lat}</li>
+          <li class= "list-group-item"><strong>longitude</strong>:${lng}</li>
+        </ul>
+        `;
     })
     .catch(function(error) {
       console.log(error);
     });
 }
 
-//
 function displayHouses(func) {
   let houses = document.querySelectorAll(".house");
-  // console.log(houses[0].attributes["1"].value)
   for (let i = 0; i < houses.length; i++) {
     locations = houses[i].attributes["1"].value.toString();
     axios
@@ -155,9 +106,7 @@ function displayHouses(func) {
         }
       })
       .then(function(response) {
-        //  show object of address
         console.log(response);
-
         //geo
         latitude = response.data.results[0].geometry.location.lat;
         long = response.data.results[0].geometry.location.lng;
